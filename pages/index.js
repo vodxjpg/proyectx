4ad2b6e64022bdd708048f78e6d399a5ae27bbe2
Vodx <vodx@proton.me>
2025-02-22 16:35:26 +0100

    Initial commit - setup NextJS project

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// pages/index.js
+export default function Home() {
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>Welcome to Trapigram!</h1>
+        <p>Your journey to building a platform begins here.</p>
+      </div>
+    );
+  }
+  
